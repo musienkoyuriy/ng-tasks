38cd02f7c09159db84245bf6f6ff0c999c763316
Yuriy Musienko <musienkoyuriy@gmail.com>
2016-01-18 16:32:44 +0200

    Update addTaskForm.js

--- src/client/directives/addTaskForm.js
+++ src/client/directives/addTaskForm.js
@@ -1,16 +1,16 @@
 class AddTaskForm {
   constructor() {
-    this.restrict = 'A';
-    this.replace = true;
-    this.templateUrl = 'add-task-form.html';
-    this.controller = 'CreateTaskController';
-    this.controllerAs = 'createTaskCtrl';
+    this.restrict = 'A'
+    this.replace = true
+    this.templateUrl = 'add-task-form.html'
+    this.controller = 'CreateTaskController'
+    this.controllerAs = 'createTaskCtrl'
   }
 }
 
 AddTaskForm.instance = () => {
-  const instance = new AddTaskForm();
-  return instance;
+  const instance = new AddTaskForm()
+  return instance
 }
 
-export default AddTaskForm;
+export default AddTaskForm
