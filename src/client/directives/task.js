fc104fe2df4e525e165050e8a38f68d63f4525b9
Yuriy Musienko <musienkoyuriy@gmail.com>
2016-01-18 16:33:09 +0200

    Update task.js

--- src/client/directives/task.js
+++ src/client/directives/task.js
@@ -1,18 +1,18 @@
 class Task {
   constructor() {
-    this.restrict = 'A';
+    this.restrict = 'A'
     this.scope = {
       data: '='
     };
-    this.templateUrl = 'task.html';
-    this.controller = 'TaskController';
-    this.controllerAs = 'taskCtrl';
+    this.templateUrl = 'task.html'
+    this.controller = 'TaskController'
+    this.controllerAs = 'taskCtrl'
   }
 }
 
 Task.instance = () => {
-  const instance = new Task();
-  return instance;
+  const instance = new Task()
+  return instance
 }
 
-export default Task;
+export default Task
